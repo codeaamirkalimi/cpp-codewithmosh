a5843b380d58a2829a947f0ee2c33014cc162953
Aamir Kalimi <codekalimi@gmail.com>
2022-11-02 23:33:09 +0530

    update: test

--- dataType.cpp
+++ dataType.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main() {
-    cout << "Learn Datatype Primitive and User Defined";
+    cout << "Learn Datatype Primitive and User Defined" << endl;
+    double price = 99.99;
+    float interestRate = 3.67F;
+    long fileSize = 900000L;
+    char letter = 'A';
+    bool isValid = true;
+    auto hi = 3;
+    int number {2};
+    cout << number << endl;
+    int bNumber = 0x10101;
+    cout << bNumber;
+    // narrowing
+
     return 0;
 }
